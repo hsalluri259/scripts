9bc615812cae7b3321fc925835148a888a65f1a8
hsalluri259 <32965862+hsalluri259@users.noreply.github.com>
2018-07-06 10:14:08 -0700

    Create postal.py
    A valid postal code P have to fullfil both below requirements:
    
      P  must be a number in the range from to inclusive.
    P    must not contain more than one alternating repetitive digit pair.

---
+++ python_scripts/postal.py
@@ -0,0 +1,7 @@
+#!/opt/app/python3/bin/python3.4
+regex_integer_in_range = r"^[1-9][\d]{5}$"      # Do not delete 'r'.
+regex_alternating_repetitive_digit_pair = r"(\d)(?=\d\1)"       # Do not delete 'r'.
+import re
+P = input()
+
+print (bool(re.match(regex_integer_in_range, P)) and len(re.findall(regex_alternating_repetitive_digit_pair, P)) < 2)
