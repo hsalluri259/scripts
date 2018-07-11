5d392d331326d69f5750bad39364a6ece4307cde
hsalluri259 <32965862+hsalluri259@users.noreply.github.com>
2018-07-11 09:31:58 -0700

    Update postal_code_validation.py
    adding comments

--- python_scripts/postal_code_validation.py
+++ python_scripts/postal_code_validation.py
@@ -1,4 +1,22 @@
 #!/opt/app/python3/bin/python3.4
+"""Question:
+A valid postal code P have to fullfil both below requirements:
+
+P    must be a number in the range from to inclusive.
+P    must not contain more than one alternating repetitive digit pair.
+
+Alternating repetitive digits are digits which repeat immediately after the next digit. In other words, an alternating repetitive digit pair is formed by two equal digits that have just a single digit between them.
+
+For example:
+
+121426 # Here, 1 is an alternating repetitive digit.
+523563 # Here, NO digit is an alternating repetitive digit.
+552523 # Here, both 2 and 5 are alternating repetitive digits.
+
+Your task is to provide two regular expressions regex_integer_in_range and regex_alternating_repetitive_digit_pair. Where:
+regex_integer_in_range should match only integers range from 100000 to 999999 inclusive
+regex_alternating_repetitive_digit_pair should find alternating repetitive digits pairs in a given string."""
+#SOlution
 regex_integer_in_range = r"^[1-9][\d]{5}$"      # Do not delete 'r'.
 regex_alternating_repetitive_digit_pair = r"(\d)(?=\d\1)"       # Do not delete 'r'.
 import re
