8a90a45babf76bfc7c182c417e706d563e3b1b70
hsalluri259 <32965862+hsalluri259@users.noreply.github.com>
2018-07-26 14:34:23 -0700

    Update XML_find_score.py

--- python_scripts/XML_find_score.py
+++ python_scripts/XML_find_score.py
@@ -38,3 +38,10 @@ import xml.etree.ElementTree as etree
 def get_attr_number(node):
     count=len(node.attrib)
     return count+sum(get_attr_number(child) for child in node)
+
+if __name__ == '__main__':
+    sys.stdin.readline()
+    xml = sys.stdin.read()
+    tree = etree.ElementTree(etree.fromstring(xml))
+    root = tree.getroot()
+    print(get_attr_number(root))
