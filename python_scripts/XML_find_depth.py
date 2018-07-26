929a21355dd239e9da51052ff00297a37f567795
hsalluri259 <32965862+hsalluri259@users.noreply.github.com>
2018-07-26 14:23:19 -0700

    Create XML_find_depth.py
    creating a python script to find out depth of an XML file

---
+++ python_scripts/XML_find_depth.py
@@ -0,0 +1,21 @@
+import xml.etree.ElementTree as etree
+maxdepth = -1
+def depth(elem, level):
+    global maxdepth
+    # your code goes here
+    if (level == maxdepth):
+        maxdepth += 1
+        
+    for child in elem:
+        depth(child, level + 1)
+ """You are given a valid XML document, and you have to print the maximum level of nesting in it. Take the depth of the root as 0.
+
+Input Format
+
+The first line contains , the number of lines in the XML document.
+The next lines follow containing the XML document.
+
+Output Format
+
+Output a single line, the integer value of the maximum level of nesting in the XML document."""   
+     
