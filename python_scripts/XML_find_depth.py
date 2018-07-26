511503523d9dc688fc0377dba0a58281ab9d973e
hsalluri259 <32965862+hsalluri259@users.noreply.github.com>
2018-07-26 14:35:10 -0700

    Update XML_find_depth.py

--- python_scripts/XML_find_depth.py
+++ python_scripts/XML_find_depth.py
@@ -9,7 +9,16 @@ def depth(elem, level):
         
     for child in elem:
         depth(child, level + 1)
- """You are given a valid XML document, and you have to print the maximum level of nesting in it. Take the depth of the root as 0.
+if __name__ == '__main__':
+    n = int(input())
+    xml = ""
+    for i in range(n):
+        xml =  xml + input() + "\n"
+    tree = etree.ElementTree(etree.fromstring(xml))
+    depth(tree.getroot(), -1)
+    print(maxdepth)
+        
+        """You are given a valid XML document, and you have to print the maximum level of nesting in it. Take the depth of the root as 0.
 
 Input Format
 
